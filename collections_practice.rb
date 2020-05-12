c604ab1e9edc6a55ce7c2c0591b22bbe9ec40272
Michael Bade <mbade314@gmail.com>
2020-05-12 23:27:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
 
+def sort_array_asc(array)
+  sorted = array.sort do |a, b|
+    a <=> b
+    end
+    return sorted
+end
+
+def sort_array_desc(array)
+  sorted = array.sort do |a, b|
+    a <=> b
+  end
+  return sorted.reverse
+end
+
+def sort_array_char_count(array)
+  sorted = array.sort do |a, b|
+    a.length <=> b.length
+  end
+  return sorted
+end
+
+def swap_elements(array)
+end
